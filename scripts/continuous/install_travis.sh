5de23a0ab6017e54dc9f3b0434e132ab844899bf
SlesarevIlya <slesarev.pr@gmail.com>
2017-06-27 15:37:08 +0300

    Create install_travis.sh

---
+++ scripts/continuous/install_travis.sh
@@ -0,0 +1,4 @@
+travis_retry sudo apt-get install -qq gcc-arm-embedded
+travis_retry sudo apt-get install -qq gcc-multilib
+travis_retry sudo apt-get install -qq u-boot-tools
+travis_retry sudo apt-get install -qq qemu qemu-system
