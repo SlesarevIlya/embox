67e0b092ac7dc7009c6159f07fe2206fb9a54844
Slesarev Ilya <slesarev.pr@gmail.com>
2017-06-28 10:42:40 +0300

    edit install-travis with if

--- scripts/continuous/install-travis.sh
+++ scripts/continuous/install-travis.sh
@@ -1,6 +1,32 @@
 TEMPLATE="$1"
+if TEMPLATE = arm/qemu
+then
+  sudo apt-get install -qq gcc-arm-embedded  
+  sudo apt-get install -qq gcc-multilib
+  sudo apt-get install -qq u-boot-tools
+  sudo apt-get install -qq qemu qemu-system
+fi
 
-sudo apt-get install -qq gcc-arm-embedded
-sudo apt-get install -qq gcc-multilib
-sudo apt-get install -qq u-boot-tools
-sudo apt-get install -qq qemu qemu-system
+if TEMPLATE = arm/stm32_f4
+then
+  sudo apt-get install -qq gcc-arm-embedded  
+  sudo apt-get install -qq gcc-multilib
+  sudo apt-get install -qq u-boot-tools
+  sudo apt-get install -qq qemu qemu-system
+fi
+
+if TEMPLATE = x86/qemu
+then
+  sudo apt-get install -qq gcc-arm-embedded  
+  sudo apt-get install -qq gcc-multilib
+  sudo apt-get install -qq u-boot-tools
+  sudo apt-get install -qq qemu qemu-system
+fi
+
+if TEMPLATE = x86/test/lang
+then
+  sudo apt-get install -qq gcc-arm-embedded  
+  sudo apt-get install -qq gcc-multilib
+  sudo apt-get install -qq u-boot-tools
+  sudo apt-get install -qq qemu qemu-system
+fi
