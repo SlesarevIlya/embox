6feb708d86afe0484a3b12fb60eba48926156efe
SlesarevIlya <slesarev.pr@gmail.com>
2017-06-29 02:01:41 +0300

    Update install-travis.sh

--- scripts/continuous/install-travis.sh
+++ scripts/continuous/install-travis.sh
@@ -1,26 +1,22 @@
 TEMPLATE="$1"
 
-if [ $TEMPLATE == arm/qemu ]
-then
+case $TEMPLATE in
+arm/*)
   sudo apt-get install -qq gcc-arm-embedded  
   sudo apt-get install -qq qemu qemu-system
-fi
 
-if [ $TEMPLATE == arm/stm32_f4 ]
-then
-  sudo apt-get install -qq gcc-arm-embedded  
-fi
+
 
 if [ $TEMPLATE == x86/qemu ]
 then
   sudo apt-get install -qq gcc-arm-embedded
   sudo apt-get install -qq gcc-multilib
   sudo apt-get install -qq qemu qemu-system
-  
 fi
 
 if [ $TEMPLATE == x86/test/lang ]
 then
   sudo apt-get install -qq gcc-arm-embedded
   sudo apt-get install -qq gcc-multilib
+  sudo apt-get install -qq qemu qemu-system
 fi
