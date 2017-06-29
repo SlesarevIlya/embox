db694740d0700577178635e8b94df22db1a506de
SlesarevIlya <slesarev.pr@gmail.com>
2017-06-29 23:26:56 +0300

    Update install-travis.sh

--- scripts/continuous/install-travis.sh
+++ scripts/continuous/install-travis.sh
@@ -13,12 +13,7 @@ x86/*)
 microblaze/*)
   sudo apt-get install -qq microblaze-elf-gcc
   ;;
-sparc/*)
-  wget https://github.com/embox/crosstool/releases/download/2.28-6.3.0-7.12/sparc-elf-toolchain.tar.bz2
-  tar axf sparc-elf-toolchain.tar.bz2
-  export PATH=$PATH:sparc-elf-toolchain.tar.bz2/bin
-  sudo apt-get install -qq sparc-elf-gcc
-  ;;
+
 mipc/*)
   sudo apt-get install -qq mipc-elf-gcc
   ;;
