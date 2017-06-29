9adb0e7e3e5d1cd89a1de0162109deb020fc33b7
SlesarevIlya <slesarev.pr@gmail.com>
2017-06-29 23:53:40 +0300

    Update install-travis.sh

--- scripts/continuous/install-travis.sh
+++ scripts/continuous/install-travis.sh
@@ -14,10 +14,6 @@ microblaze/*)
   sudo apt-get install -qq microblaze-elf-gcc
   ;;
 sparc/*)
-  sudo apt-get install -qq gcc-arm-embedded
-  sudo apt-get install -qq gcc-multilib
-  sudo apt-get install -qq u-boot-tools
-  sudo apt-get install -qq qemu qemu-system
   wget https://github.com/embox/crosstool/releases/download/2.28-6.3.0-7.12/sparc-elf-toolchain.tar.bz2
   tar axf sparc-elf-toolchain.tar.bz2
   export PATH=$PATH:sparc-elf-toolchain.tar.bz2/bin
