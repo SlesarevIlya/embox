f3a3d1c7f4b920c2cb421162594b834ec9402315
SlesarevIlya <slesarev.pr@gmail.com>
2017-06-30 00:49:25 +0300

    Update install-travis.sh

--- scripts/continuous/install-travis.sh
+++ scripts/continuous/install-travis.sh
@@ -15,7 +15,7 @@ microblaze/*)
   tar axf microblaze-elf-toolchain.tar.bz2
   export PATH=$PATH:microblaze-elf-toolchain/bin
   ;;
-sparc/debug)
+sparc/*)
   wget https://github.com/embox/crosstool/releases/download/2.28-6.3.0-7.12/sparc-elf-toolchain.tar.bz2
   tar axf sparc-elf-toolchain.tar.bz2
   export PATH=$PATH:sparc-elf-toolchain/bin
