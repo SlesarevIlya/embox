cfaa02f12c116b9b6b8fa0228b79aee1edadb087
SlesarevIlya <slesarev.pr@gmail.com>
2017-06-30 09:40:34 +0300

    Update install-travis.sh

--- scripts/continuous/install-travis.sh
+++ scripts/continuous/install-travis.sh
@@ -10,24 +10,24 @@ x86/*)
   sudo apt-get install -qq gcc-multilib
   sudo apt-get install -qq qemu qemu-system
   ;;
-microblaze/*)
+microblaze/qemu)
   wget https://github.com/embox/crosstool/releases/download/2.28-6.3.0-7.12/microblaze-elf-toolchain.tar.bz2
   tar axf microblaze-elf-toolchain.tar.bz2
-  export PATH=$PATH:microblaze-elf-toolchain/bin
+  export PATH=$PATH:$PWD"/microblaze-elf-toolchain/bin/"
   ;;
-sparc/*)
+sparc/qemu)
   wget https://github.com/embox/crosstool/releases/download/2.28-6.3.0-7.12/sparc-elf-toolchain.tar.bz2
   tar axf sparc-elf-toolchain.tar.bz2
-  export PATH=$PATH:sparc-elf-toolchain/bin
+  export PATH=$PATH:$PWD"/sparc-elf-toolchain/bin/"
   ;;
-mipc/*)
+mipc/qemu)
   wget https://github.com/embox/crosstool/releases/download/2.28-6.3.0-7.12/mipc-elf-toolchain.tar.bz2
   tar axf mipc-elf-toolchain.tar.bz2
-  export PATH=$PATH:mipc-elf-toolchain/bin
+  export PATH=$PATH:$PWD"/mipc-elf-toolchain/bin/"
   ;;
-ppc/*)
+ppc/qemu)
   wget https://github.com/embox/crosstool/releases/download/2.28-6.3.0-7.12/powerpc-elf-toolchain.tar.bz2
   tar axf powerpc-elf-toolchain.tar.bz2
-  export PATH=$PATH:powerpc-elf-toolchain/bin
+  export PATH=$PATH:$PWD"/powerpc-elf-toolchain/bin/"
   ;;
 esac
